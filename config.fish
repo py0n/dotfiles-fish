2c9671d9157e2bd8df2e191cd904ed8f55d4273c
ISHIKAWA YUICHIRO <py.o.n.13+git@gmail.com>
2017-11-29 08:44:56 +0900

    ndenv用の設定を追加

--- config.fish
+++ config.fish
@@ -71,6 +71,25 @@ if status --is-interactive
     end
     # }}}
 
+    # ndenv {{{
+        set -gx PATH $HOME/.ndenv/shims $PATH
+        # 'ndenv init -' がfishに対応していないので茲に内容を記載
+        # https://github.com/riywo/ndenv/pull/14
+        # https://github.com/riywo/ndenv/pull/15
+        # など、PRが出てゐるが取り込まれてゐない。
+        command ndenv rehash 2> /dev/null
+        function ndenv
+            set command $argv[1]
+            set -e argv[1]
+            switch "$command"
+            case rehash shell
+                source (ndenv "sh-$command" $argv|psub)
+            case '*'
+                command ndenv "$command" $argv
+            end
+        end
+    # }}}
+
     # Remove duplicate elements from PATH {{{
     set -gx PATH (for i in $PATH; echo $i; end | awk '!a[$0]++{print}')
     # }}}
