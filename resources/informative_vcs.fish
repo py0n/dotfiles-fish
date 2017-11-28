9abd1ad2bb50c74c78630d1160ffaee7adc119a0
ISHIKAWA YUICHIRO <py.o.n.13+git@gmail.com>
2017-11-29 08:54:53 +0900

    git promptの文字を変更

--- resources/informative_vcs.fish
+++ resources/informative_vcs.fish
@@ -18,29 +18,29 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_showupstream "informative"
     end
     if not set -q __fish_git_prompt_char_upstream_ahead
-        set -g __fish_git_prompt_char_upstream_ahead "a"
+        set -g __fish_git_prompt_char_upstream_ahead "↑"
     end
     if not set -q __fish_git_prompt_char_upstream_behind
-        set -g __fish_git_prompt_char_upstream_behind "b"
+        set -g __fish_git_prompt_char_upstream_behind "↓"
     end
     if not set -q __fish_git_prompt_char_upstream_prefix
         set -g __fish_git_prompt_char_upstream_prefix ""
     end
 
     if not set -q __fish_git_prompt_char_stagedstate
-        set -g __fish_git_prompt_char_stagedstate "o"
+        set -g __fish_git_prompt_char_stagedstate "●"
     end
     if not set -q __fish_git_prompt_char_dirtystate
-        set -g __fish_git_prompt_char_dirtystate "+"
+        set -g __fish_git_prompt_char_dirtystate "✚"
     end
     if not set -q __fish_git_prompt_char_untrackedfiles
-        set -g __fish_git_prompt_char_untrackedfiles "!"
+        set -g __fish_git_prompt_char_untrackedfiles "…"
     end
     if not set -q __fish_git_prompt_char_conflictedstate
-        set -g __fish_git_prompt_char_conflictedstate "x"
+        set -g __fish_git_prompt_char_conflictedstate "✖"
     end
     if not set -q __fish_git_prompt_char_cleanstate
-        set -g __fish_git_prompt_char_cleanstate "-"
+        set -g __fish_git_prompt_char_cleanstate "✔"
     end
 
     if not set -q __fish_git_prompt_color_dirtystate
