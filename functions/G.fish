72383484badac0a0eedb882b06a74366ecf504a0
Pyon <py0n@users.noreply.github.com>
2017-12-02 13:44:46 +0900

    該當ファイルが無い時はエディタを開かない

--- functions/G.fish
+++ functions/G.fish
@@ -1,8 +1,10 @@
-# Defined in /tmp/fish.4HUQnF/G.fish @ line 2
+# Defined in /tmp/fish.IPuNi1/G.fish @ line 2
 function G --description 'narrow down git-grep' --argument query directory
 	if test -z "$directory"
 		set directory '.'
 	end
 	set -l array ( git grep -i -n $query $directory | fzf | string split -m2 ':' )[1..2]
-	nvim "+$array[2]" "$array[1]"
+	if count $array
+		nvim "+$array[2]" "$array[1]"
+	end
 end
