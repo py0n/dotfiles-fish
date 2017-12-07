f9b926d1b4fb47631a7721c551873e409b1393d8
ISHIKAWA YUICHIRO <py.o.n.13+git@gmail.com>
2017-12-07 14:42:22 +0900

    余計な範囲指定を削除

--- functions/G.fish
+++ functions/G.fish
@@ -1,9 +1,9 @@
-# Defined in /tmp/fish.IPuNi1/G.fish @ line 2
+# Defined in /tmp/fish.WNfYUm/G.fish @ line 2
 function G --description 'narrow down git-grep' --argument query directory
 	if test -z "$directory"
 		set directory '.'
 	end
-	set -l array ( git grep -i -n $query $directory | fzf | string split -m2 ':' )[1..2]
+	set -l array (git grep -i -n $query $directory | fzf | string split -m2 ':')
 	if count $array
 		nvim "+$array[2]" "$array[1]"
 	end
