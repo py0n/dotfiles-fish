fb322a8430e5109982bd563211fd281f96767fab
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-19 19:08:35 +0900

    引数が無いときは何もしない

--- functions/G.fish
+++ functions/G.fish
@@ -1,10 +1,12 @@
-# Defined in /tmp/fish.WNfYUm/G.fish @ line 2
-function G --description 'narrow down git-grep' --argument query directory
-	if test -z "$directory"
-		set directory '.'
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.Is8KjH/G.fish @ line 2
+function G --description 'narrow down git-grep'
+	if not count $argv >/dev/null
+		return 1
 	end
-	set -l array (git grep -i -n $query $directory | fzf | string split -m2 ':')
-	if count $array
+
+	set -l array (git grep -i -n $argv | fzf | string split -m2 ':')
+
+	if count $array >/dev/null
 		nvim "+$array[2]" "$array[1]"
 	end
 end
