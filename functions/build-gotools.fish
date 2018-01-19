d1cd2a72009870060fcc360e77fd8e797971384c
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-19 09:22:43 +0900

    必要なGoツールを入れるコマンドを追加

---
+++ functions/build-gotools.fish
@@ -0,0 +1,8 @@
+# Defined in /tmp/fish.9H7FJQ/build-gotools.fish @ line 2
+function build-gotools --description 'Build pt, etc.'
+	go get -u github.com/d4l3k/go-pry
+	go get -u github.com/monochromegane/the_platinum_searcher/...
+	go get -u github.com/motemen/ghq
+	go get -u github.com/nsf/gocode
+	go get -u golang.org/x/tools/cmd/...
+end
