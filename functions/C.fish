6eeb21f6dc5236b6233d199e2f6bd9bc6eda7603
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-19 15:08:58 +0900

    極力fishのコマンドを使用する

--- functions/C.fish
+++ functions/C.fish
@@ -1,4 +1,4 @@
-# Defined in /home/yuichirou.ishikawa/.config/fish/functions/c.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.dPbxqM/C.fish @ line 2
 function C --description 'fzf git-commit'
-	echo -n ( git log --full-history --date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%h [%ad] %an : %s' | fzf | awk '{print $1}' )
+	echo -n ( git log --full-history --date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%h [%ad] %an : %s' | fzf | cut -d ' ' -f 1 )
 end
