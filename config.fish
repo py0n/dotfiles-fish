ba67ec759158ce0f3b301690b90df6834584307d
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-19 12:06:42 +0900

    ちょっと修正

--- config.fish
+++ config.fish
@@ -1,18 +1,18 @@
 # from share/fish/config.fish
 set -g configdir ~/.config
+
+set -q XDG_CONFIG_HOME; and set configdir $XDG_CONFIG_HOME
+
 set -g userdatadir ~/.local/share
 
-# environments {{{
-set -q XDG_CONFIG_HOME; and set configdir   $XDG_CONFIG_HOME
-set -q XDG_DATA_HOME;   and set userdatadir $XDG_DATA_HOME
-# }}}
+set -q XDG_DATA_HOME; and set userdatadir $XDG_DATA_HOME
 
 # Bash on Ubuntu on Windows
 if string match -q -r 'Linux' (uname -a); and string match -q -r 'Microsoft' (uname -a)
     umask 0022
 end
 
-# environments (at interactive) {{{
+# environments (at interactive shell) {{{
 if status --is-interactive
     # EDITOR
     type -q vim;  and set -gx EDITOR vim
@@ -27,9 +27,8 @@ if status --is-interactive
 end
 # }}}
 
-# interactive shell {{{
+# ssh-agent (at interactive shell){{
 if status --is-interactive
-    # ssh-agent {{{
     set -l SSH_AUTH_SOCK_SYMLINK $HOME/.ssh-agent-$USER
     if set -q SSH_AUTH_SOCK; and test -S $SSH_AUTH_SOCK; and test ! -L $SSH_AUTH_SOCK
         ln -sfn $SSH_AUTH_SOCK $SSH_AUTH_SOCK_SYMLINK
@@ -37,16 +36,17 @@ if status --is-interactive
     else if test -S $SSH_AUTH_SOCK_SYMLINK
         set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK_SYMLINK
     end
-
     command ssh-add -L >/dev/null ^&1
     if test $status -ne '0'
         eval (ssh-agent -c | grep '^setenv')
         ln -sfn $SSH_AUTH_SOCK $SSH_AUTH_SOCK_SYMLINK
         set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK_SYMLINK
     end
-    # }}}
+end
+# }}}
 
-    # resources
+# resources (at interactive shell) {{{
+if status --is-interactive
     for f in {$configdir}/fish/resources/*.fish
         source $f
     end
@@ -67,7 +67,7 @@ if status --is-interactive
 end
 # }}}
 
-# *env {{{
+# *env (at interactive shell) {{{
 # goenv {{{
 # https://qiita.com/spiegel-im-spiegel/items/c140fc1fe02aa24be4fd
 if status --is-interactive; and test -d $HOME/.goenv
@@ -107,7 +107,7 @@ end
 # }}}
 # }}}
 
-# Remove duplicate elements from PATH {{{
+# Remove duplicate elements from PATH (at interactive shell) {{{
 if status --is-interactive
     set -gx PATH (for i in $PATH; echo $i; end | awk '!a[$0]++{print}')
 end
