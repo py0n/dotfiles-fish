ea220cb2d4e4d2141ff826b6d18b9bc01ddad1e8
ISHIKAWA YUICHIRO <py.o.n.13+git@gmail.com>
2018-01-19 10:08:30 +0900

    goreも使ってみる

--- functions/build-gotools.fish
+++ functions/build-gotools.fish
@@ -1,8 +1,9 @@
-# Defined in /tmp/fish.9H7FJQ/build-gotools.fish @ line 2
+# Defined in /Users/yuichirou.ishikawa/.config/fish/functions/build-gotools.fish @ line 2
 function build-gotools --description 'Build pt, etc.'
 	go get -u github.com/d4l3k/go-pry
 	go get -u github.com/monochromegane/the_platinum_searcher/...
 	go get -u github.com/motemen/ghq
+	go get -u github.com/motemen/gore
 	go get -u github.com/nsf/gocode
 	go get -u golang.org/x/tools/cmd/...
 end
