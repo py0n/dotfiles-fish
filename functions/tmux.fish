cc30fbe370bdd3d686b3f725f9b3c0d4f2cbf27f
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-19 14:03:30 +0900

    tmuxが無いときは終了

--- functions/tmux.fish
+++ functions/tmux.fish
@@ -1,13 +1,15 @@
-# Defined in /tmp/fish.gPio6a/tmux.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.Jo0vuq/tmux.fish @ line 2
 function tmux --description 'tmux with auto-attaching'
-	if type -p tmux
-		set -l n (count $argv)
-		if test $n -gt 0
-			command tmux $argv
-		else if command tmux has-session
-			command tmux attach
-		else
-			command tmux -2
-		end
+	if not type -f -p -q tmux
+		return 1
+	end
+
+	set -l n (count $argv)
+	if test $n -gt 0
+		command tmux $argv
+	else if command tmux has-session
+		command tmux attach
+	else
+		command tmux -2
 	end
 end
