94acf52bea31e702096c1a60ad5a06dd6cf89bac
ISHIKAWA YUICHIRO <py.o.n.13+git@gmail.com>
2018-01-19 10:52:09 +0900

    MacOSのawkではalnumが効かなかったので修正

--- functions/slogin.fish
+++ functions/slogin.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.Aov2Gz/slogin.fish @ line 1
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.VfbF7l/slogin.fish @ line 2
 function slogin --description 'slogin with fzf & ssh-config'
 	if count $argv >/dev/null
 		command slogin $argv
 	else
-		slogin (awk '/^Host[[:space:]]+[[:alnum:]_\-]+$/ { print $2 }' $HOME/.ssh/config | fzf)
+        slogin (awk '/^Host[[:space:]]+[A-Za-z0-9_\.\-]+$/ { print $2 }' $HOME/.ssh/config | fzf)
 	end
 end
