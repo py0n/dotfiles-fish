c8fa4477050a17c3064e1fb5410bb528b45f1c1d
Pyon <py0n@users.noreply.github.com>
2018-01-20 11:42:11 +0900

    go toolを追加

--- functions/build-gotools.fish
+++ functions/build-gotools.fish
@@ -1,6 +1,11 @@
-# Defined in /Users/yuichirou.ishikawa/.config/fish/functions/build-gotools.fish @ line 2
+# Defined in /tmp/fish.dbVRxG/build-gotools.fish @ line 2
 function build-gotools --description 'Build pt, etc.'
+	go get -u github.com/Songmu/make2help/cmd/make2help
 	go get -u github.com/d4l3k/go-pry
+	go get -u github.com/davecgh/go-spew/spew
+	go get -u github.com/golang/dep/cmd/dep
+	go get -u github.com/golang/lint/golint
+	go get -u github.com/k0kubun/pp
 	go get -u github.com/monochromegane/the_platinum_searcher/...
 	go get -u github.com/motemen/ghq
 	go get -u github.com/motemen/gore
