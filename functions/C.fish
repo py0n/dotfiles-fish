62e7ca020bb0eb748744557e5da0ff5e27c53701
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-23 18:50:19 +0900

    文字列操作をfishのみで行う

--- functions/C.fish
+++ functions/C.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.8MA89Z/C.fish @ line 2
+# Defined in /tmp/fish.ISUn9l/C.fish @ line 2
 function C --description 'fzf git-commit'
-	echo -n ( git log --full-history --date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%h [%ad] %an : %s %d' | fzf | cut -d ' ' -f 1 )
+	echo -n (git log --full-history --date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%h [%ad] %an : %s %d' | fzf | string split ' ')[1]
 end
