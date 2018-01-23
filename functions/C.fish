ccf1b1e9f3fdb74e1a019931c63b0dc0accc18a2
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-23 18:46:12 +0900

    各コミットのref nameも表示する

--- functions/C.fish
+++ functions/C.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.dPbxqM/C.fish @ line 2
+# Defined in /tmp/fish.8MA89Z/C.fish @ line 2
 function C --description 'fzf git-commit'
-	echo -n ( git log --full-history --date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%h [%ad] %an : %s' | fzf | cut -d ' ' -f 1 )
+	echo -n ( git log --full-history --date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%h [%ad] %an : %s %d' | fzf | cut -d ' ' -f 1 )
 end
