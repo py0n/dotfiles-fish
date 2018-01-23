24604eacc7f18dbc5e7e680312fd8c68a735dfbd
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-23 18:45:37 +0900

    現在のブランチ名に付くアスタリスクを削除する

--- functions/B.fish
+++ functions/B.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.1Osmrj/B.fish @ line 2
+# Defined in /tmp/fish.JOuVZb/B.fish @ line 2
 function B --description 'fzf git-branch'
-	echo -n ( git branch --all | string trim | string replace 'remotes/' '' | fzf )
+	echo -n ( git branch --all | string trim | string replace -r '^(remotes/|\* )' '' | fzf )
 end
