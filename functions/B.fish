76014d9b75463b8c437606ee303fde04ba3e8711
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-23 18:58:09 +0900

    ブランチの説明文も表示する

--- functions/B.fish
+++ functions/B.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.JOuVZb/B.fish @ line 2
+# Defined in /tmp/fish.L21HLH/B.fish @ line 2
 function B --description 'fzf git-branch'
-	echo -n ( git branch --all | string trim | string replace -r '^(remotes/|\* )' '' | fzf )
+	echo -n (git branch -avv | string trim | string replace -r '^(remotes/|\* )' '' | fzf | string split ' ')[1]
 end
