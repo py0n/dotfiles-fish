f02f9818795753db6a8c9a9449074ecc485fa4c4
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-01-24 18:47:40 +0900

    リポジトリを移動するコマンドを修正

--- functions/R.fish
+++ functions/R.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.wB58A7/R.fish @ line 2
+# Defined in /tmp/fish.cssELv/R.fish @ line 2
 function R --description 'fzf ghq list'
-	echo -n (git config --global ghq.root)/(ghq list | fzf)
+	echo -n (ghq root)/(ghq list | fzf)
 end
--- functions/cr.fish
+++ functions/cr.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.IaoD5R/cr.fish @ line 2
+# Defined in /tmp/fish.BeO0d6/cr.fish @ line 2
 function cr --description 'change repository'
-	cd (git config --global ghq.root)/(ghq list | fzf)
+	cd (R)
 end
