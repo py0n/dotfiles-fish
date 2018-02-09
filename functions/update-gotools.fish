04558da0c25d449ec2682f1767e6a2dccdf90241
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-02-09 09:55:48 +0900

    refactor: 関数名を変更

--- functions/update-gotools.fish
+++ functions/update-gotools.fish
@@ -1,5 +1,5 @@
-# Defined in /tmp/fish.1VpogV/build-gotools.fish @ line 2
-function build-gotools --description 'Build pt, etc.'
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.M8fSf6/update-gotools.fish @ line 1
+function update-gotools --description 'update tools of go'
 	go get -u github.com/Songmu/make2help/cmd/make2help
 	go get -u github.com/d4l3k/go-pry
 	go get -u github.com/davecgh/go-spew/spew
