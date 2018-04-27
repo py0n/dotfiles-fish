269e86306780ff5e14ad1a47b0f0767835fe1e83
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-04-27 17:24:22 +0900

    update: PATHの取った場所に git-sync が無ければ作成する

--- functions/git-sync.fish
+++ functions/git-sync.fish
@@ -1,40 +1,41 @@
-# Defined in /tmp/fish.6VsvPc/git-sync.fish @ line 2
+# Defined in /tmp/fish.H9yeDE/git-sync.fish @ line 2
 function git-sync
-	if count $argv >/dev/null 2>&1; [ $argv[1] = "init" ]
-        mkdir -p $HOME/.local/bin
+	# setup
+	if type --no-function --quiet git-sync; or true
+        command mkdir -p $HOME/.local/bin
         echo -e "#!/bin/sh\nfish -c git-sync" > $HOME/.local/bin/git-sync
-        chmod +x $HOME/.local/bin/git-sync
-    else
-        # git-sync (fish version)
-        #
-        # cf. https://qiita.com/masarakki/items/27f2cb456b4801ccb31b
-        command git fetch --all --prune
-
-        set -l modifieds (command git status --porcelain --untracked-files=no | string length)
-
-        if count $modifieds >/dev/null 2>&1
-            command git stash
-        end
-
-        set -l current (command git branch | string match --entire --regex '^\* ' | string split ' ')[2]
-
-        command git checkout master
-        command git pull --rebase
-
-        for b in (command git branch --no-color --merged | string match --entire --invert --regex '^\* ' | string trim)
-            command git branch -d $b
-        end
-
-        for b in (command git branch --no-color -vv | string match --entire --regex '\[[A-Za-z0-9_\/\.\-]*: gone\]' | string trim)
-            command git branch -D  (string split --max 1 ' ' $b)[1]
-        end
-
-        if count $modifieds >/dev/null 2>&1
-            if command git checkout $current
-                command git stash pop
-            else
-                command git stash drop
-            end
-        end
+        command chmod +x $HOME/.local/bin/git-sync
     end
+
+	# git-sync (fish version)
+	#
+	# cf. https://qiita.com/masarakki/items/27f2cb456b4801ccb31b
+	command git fetch --all --prune
+
+	set -l modifieds (command git status --porcelain --untracked-files=no | string length)
+
+	if count $modifieds >/dev/null 2>&1
+		command git stash
+	end
+
+	set -l current (command git branch | string match --entire --regex '^\* ' | string split ' ')[2]
+
+	command git checkout master
+	command git pull --rebase
+
+	for b in (command git branch --no-color --merged | string match --entire --invert --regex '^\* ' | string trim)
+		command git branch -d $b
+	end
+
+	for b in (command git branch --no-color -vv | string match --entire --regex '\[[A-Za-z0-9_\/\.\-]*: gone\]' | string trim)
+		command git branch -D  (string split --max 1 ' ' $b)[1]
+	end
+
+	if count $modifieds >/dev/null 2>&1
+		if command git checkout $current
+			command git stash pop
+		else
+			command git stash drop
+		end
+	end
 end
