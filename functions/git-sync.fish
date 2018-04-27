5ecbdf19de68adcecf1a90bd027000a6df261c39
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-04-27 16:25:41 +0900

    update: use original git command

--- functions/git-sync.fish
+++ functions/git-sync.fish
@@ -1,34 +1,34 @@
-# Defined in /tmp/fish.P5Ob9Y/git-sync.fish @ line 2
+# Defined in /tmp/fish.qQ5RLi/git-sync.fish @ line 2
 function git-sync
 	# git-sync (fish version)
 	#
 	# cf. https://qiita.com/masarakki/items/27f2cb456b4801ccb31b
-	git fetch --all --prune
+	command git fetch --all --prune
 
-	set -l modifieds (git status --porcelain --untracked-files=no | string length)
-
-	if count $modifieds
-		git stash
-	end
-
-	set -l current (git branch | string match --entire --regex '^\* ' | string split ' ')[2]
-
-	git checkout master
-	git pull --rebase
-
-	for b in (git branch --no-color --merged | string match --entire --invert --regex '^\* ' | string trim)
-		git branch -d $b
-	end
-
-	for b in (git branch --no-color -vv | string match --entire --regex '\[[A-Za-z0-9_\/\.\-]*: gone\]' | string trim)
-		git branch -D  (string split --max 1 ' ' $b)[1]
-	end
+	set -l modifieds (command git status --porcelain --untracked-files=no | string length)
 
 	if count $modifieds
-		if git checkout $current
-			git stash pop
+		command git stash
+	end
+
+	set -l current (command git branch | string match --entire --regex '^\* ' | string split ' ')[2]
+
+	command git checkout master
+	command git pull --rebase
+
+	for b in (command git branch --no-color --merged | string match --entire --invert --regex '^\* ' | string trim)
+		command git branch -d $b
+	end
+
+	for b in (command git branch --no-color -vv | string match --entire --regex '\[[A-Za-z0-9_\/\.\-]*: gone\]' | string trim)
+		command git branch -D  (string split --max 1 ' ' $b)[1]
+	end
+
+	if count $modifieds
+		if command git checkout $current
+			command git stash pop
 		else
-			git stash drop
+			command git stash drop
 		end
 	end
 end
