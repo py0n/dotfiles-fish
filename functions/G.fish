6b2492fcdcac22095d9ffd9f8f30a89c2737c5b4
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2018-10-30 16:08:27 +0900

    update: use cycyle options

--- functions/G.fish
+++ functions/G.fish
@@ -1,12 +1,12 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.Is8KjH/G.fish @ line 2
+# Defined in /tmp/fish.WsYMwi/G.fish @ line 2
 function G --description 'narrow down git-grep'
 	if not count $argv >/dev/null
 		return 1
 	end
 
-	set -l array (git grep -i -n $argv | fzf | string split -m2 ':')
+	set -l array (command git grep -i -n $argv | fzf --ansi --cycle | string split -m2 ':')
 
 	if count $array >/dev/null
-		nvim "+$array[2]" "$array[1]"
+		command nvim "+$array[2]" "$array[1]"
 	end
 end
