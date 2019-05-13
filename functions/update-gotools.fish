1dea7766c7bef848b64f5878ef0c9ce5052cadf1
ISHIKAWA YUICHIRO <py0n@users.noreply.github.com>
2019-05-13 11:05:49 +0900

    update: golint のリポジトリを変更

--- functions/update-gotools.fish
+++ functions/update-gotools.fish
@@ -1,10 +1,10 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.M8fSf6/update-gotools.fish @ line 1
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.Zbij7Q/update-gotools.fish @ line 2
 function update-gotools --description 'update tools of go'
 	go get -u github.com/Songmu/make2help/cmd/make2help
 	go get -u github.com/d4l3k/go-pry
 	go get -u github.com/davecgh/go-spew/spew
 	go get -u github.com/golang/dep/cmd/dep
-	go get -u github.com/golang/lint/golint
+	go get -u golang.org/x/lint/golint
 	go get -u github.com/jstemmer/gotags
 	go get -u github.com/k0kubun/pp
 	go get -u github.com/monochromegane/the_platinum_searcher/...
