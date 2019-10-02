411e37a0024fe5b57a6055df7b76ec8f72d5a3dd
py0n <py0n@users.noreply.github.com>
2019-10-02 18:37:39 +0900

    update: fzf のオプションに --layout=reverse-list を追加する

--- functions/R.fish
+++ functions/R.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.GjSzXO/R.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.iPJ3ms/R.fish @ line 2
 function R --description 'fzf ghq list'
-	echo -n (ghq root)/(ghq list | fzf --ansi --cycle)
+	echo -n (ghq root)/(ghq list | fzf --ansi --cycle --layout=reverse-list)
 end
