12971e8409e923bcebcd58e8e05aca60141cbe7c
py0n <py0n@users.noreply.github.com>
2020-02-21 12:22:26 +0900

    update: git grepでperl互換の正規表現を使用する

--- functions/G.fish
+++ functions/G.fish
@@ -1,10 +1,10 @@
-# Defined in /tmp/fish.xSgXQ0/G.fish @ line 2
+# Defined in /tmp/fish.um41gm/G.fish @ line 2
 function G --description 'narrow down git-grep'
 	if not count $argv >/dev/null
 		return 1
 	end
 
-	set -l array (command git grep -E -i -n $argv | fzf --ansi --cycle --layout=reverse-list | string split -m2 ':')
+	set -l array (command git grep --perl-regexp -i -n $argv | fzf --ansi --cycle --layout=reverse-list | string split -m2 ':')
 
 	if count $array >/dev/null
 		command nvim "+$array[2]" "$array[1]"
