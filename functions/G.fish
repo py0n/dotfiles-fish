61b2a8e923a244b9024d58d0af01addde18d3318
py0n <py0n@users.noreply.github.com>
2020-02-26 07:52:21 +0900

    update: [fzf] fzf-tmuxが存在するときはそちらを使用する

--- functions/G.fish
+++ functions/G.fish
@@ -1,10 +1,14 @@
-# Defined in /tmp/fish.um41gm/G.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.whxQ0e/G.fish @ line 2
 function G --description 'narrow down git-grep'
 	if not count $argv >/dev/null
 		return 1
 	end
 
-	set -l array (command git grep --perl-regexp -i -n $argv | fzf --ansi --cycle --layout=reverse-list | string split -m2 ':')
+    if test --quiet --no-function fzf-tmux
+		set -l array (command git grep --perl-regexp -i -n $argv | command fzf-tmux --ansi --cycle --layout=reverse-list | string split -m2 ':')
+	else
+		set -l array (command git grep --perl-regexp -i -n $argv | command fzf --ansi --cycle --layout=reverse-list | string split -m2 ':')
+	end
 
 	if count $array >/dev/null
 		command nvim "+$array[2]" "$array[1]"
--- functions/ssh.fish
+++ functions/ssh.fish
@@ -1,8 +1,10 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.lLHkt9/ssh.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.bkvO2G/ssh.fish @ line 2
 function ssh --description 'ssh with fzf & ssh-config'
 	if count $argv >/dev/null
 		command ssh $argv
+	else if type --quiet --no-function fzf-tmux
+		command ssh (awk '/^Host[[:space:]]+[A-Za-z0-9_\.\-]+$/ { print $2 }' $HOME/.ssh/config | command fzf-tmux --ansi --cycle --layout=reverse-list)
 	else
-        ssh (awk '/^Host[[:space:]]+[A-Za-z0-9_\.\-]+$/ { print $2 }' $HOME/.ssh/config | fzf --ansi --cycle --layout=reverse-list)
+		command ssh (awk '/^Host[[:space:]]+[A-Za-z0-9_\.\-]+$/ { print $2 }' $HOME/.ssh/config | command fzf --ansi --cycle --layout=reverse-list)
 	end
 end
