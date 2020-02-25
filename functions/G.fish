271859d86221b951769223168fc5e8e7ff637f90
py0n <py0n@users.noreply.github.com>
2020-02-26 08:19:12 +0900

    update: 環境変数TMUXを確認する

--- functions/G.fish
+++ functions/G.fish
@@ -1,14 +1,15 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.WIWxcs/G.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.ZUeWdU/G.fish @ line 2
 function G --description 'narrow down git-grep'
 	if not count $argv >/dev/null
 		return 1
 	end
 
-    if test --quiet --no-functions fzf-tmux
-		set -l array (command git grep --perl-regexp -i -n $argv | command fzf-tmux --ansi --cycle --layout=reverse-list | string split -m2 ':')
+	if set --query TMUX; and type --quiet --no-functions fzf-tmux
+		set cmd_fzf fzf-tmux
 	else
-		set -l array (command git grep --perl-regexp -i -n $argv | command fzf --ansi --cycle --layout=reverse-list | string split -m2 ':')
+		set cmd_fzf fzf
 	end
+	set -l array (command git grep --perl-regexp -i -n $argv | command $cmd_fzf --ansi --cycle --layout=reverse-list | string split -m2 ':')
 
 	if count $array >/dev/null
 		command nvim "+$array[2]" "$array[1]"
--- functions/ssh.fish
+++ functions/ssh.fish
@@ -1,8 +1,8 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.bkvO2G/ssh.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.vPx3FR/ssh.fish @ line 2
 function ssh --description 'ssh with fzf & ssh-config'
 	if count $argv >/dev/null
 		command ssh $argv
-	else if type --quiet --no-function fzf-tmux
+	else if set --query TMUX; and type --quiet --no-function fzf-tmux
 		command ssh (awk '/^Host[[:space:]]+[A-Za-z0-9_\.\-]+$/ { print $2 }' $HOME/.ssh/config | command fzf-tmux --ansi --cycle --layout=reverse-list)
 	else
 		command ssh (awk '/^Host[[:space:]]+[A-Za-z0-9_\.\-]+$/ { print $2 }' $HOME/.ssh/config | command fzf --ansi --cycle --layout=reverse-list)
