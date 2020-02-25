8ddd84ba6ae04493bec098a3cca6697cbc5cd9bc
py0n <py0n@users.noreply.github.com>
2020-02-26 07:58:58 +0900

    fix: typo

--- functions/G.fish
+++ functions/G.fish
@@ -1,10 +1,10 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.whxQ0e/G.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.WIWxcs/G.fish @ line 2
 function G --description 'narrow down git-grep'
 	if not count $argv >/dev/null
 		return 1
 	end
 
-    if test --quiet --no-function fzf-tmux
+    if test --quiet --no-functions fzf-tmux
 		set -l array (command git grep --perl-regexp -i -n $argv | command fzf-tmux --ansi --cycle --layout=reverse-list | string split -m2 ':')
 	else
 		set -l array (command git grep --perl-regexp -i -n $argv | command fzf --ansi --cycle --layout=reverse-list | string split -m2 ':')
