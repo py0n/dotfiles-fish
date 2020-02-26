f2bff745c7558470c347263dd951ddd758fc20cc
py0n <py0n@users.noreply.github.com>
2020-02-27 07:47:45 +0900

    update: [fzf] fzf-tmuxが存在するときはそちらを使用する

--- functions/R.fish
+++ functions/R.fish
@@ -1,4 +1,9 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.iPJ3ms/R.fish @ line 2
+# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.38UIl6/R.fish @ line 2
 function R --description 'fzf ghq list'
-	echo -n (ghq root)/(ghq list | fzf --ansi --cycle --layout=reverse-list)
+	if set --query TMUX; and type --no-functions --quiet fzf-tmux
+		set cmd fzf-tmux
+	else
+		set cmd fzf
+	end
+	echo -n (ghq root)/(ghq list | command $cmd --ansi --cycle --layout=reverse-list)
 end
