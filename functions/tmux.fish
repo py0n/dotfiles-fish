53b1ca246ef546f83f71260fc48a463df817fed6
py0n <py0n@users.noreply.github.com>
2026-01-21 11:13:15 +0900

    modify: 4.3.3 対応 (by ChatGPT)

--- functions/tmux.fish
+++ functions/tmux.fish
@@ -1,15 +1,14 @@
-# Defined in /var/folders/jd/dp3g4h052llfp2p75h7lq4685nprdw/T//fish.nAm4cO/tmux.fish @ line 2
 function tmux --description 'tmux with auto-attaching'
-	if not type -f -q tmux
-		return 1
-	end
+    if not type -q tmux
+        return 1
+    end
 
-	set -l n (count $argv)
-	if test $n -gt 0
-		command tmux $argv
-	else if command tmux has-session
-		command tmux attach
-	else
-		command tmux -2
-	end
+    set -l n (count $argv)
+    if test $n -gt 0
+        command tmux $argv
+    else if command tmux has-session 2>/dev/null
+        command tmux attach
+    else
+        command tmux -2
+    end
 end
