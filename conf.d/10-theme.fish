fdff612e5e863ab50e5f6a3d0ea4b3837298cbcb
py0n <py0n@users.noreply.github.com>
2026-01-21 14:11:22 +0900

    modify: 色の設定を別ファイルに追い出す (by ChatGPT)

---
+++ conf.d/10-theme.fish
@@ -0,0 +1,16 @@
+# ---- テーマ / 色設定 ----
+set --global fish_color_cancel -r
+set --global fish_color_cwd_root red
+set --global fish_color_history_current --bold
+set --global fish_color_host_remote yellow
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+
+set --global fish_pager_color_completion
+set --global fish_pager_color_description B3A06D yellow
+set --global fish_pager_color_prefix white --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_selected_background -r
+# -------------------------
--- config.fish
+++ config.fish
@@ -50,22 +50,6 @@ status --is-interactive; or exit
 # キーバインド（デフォルトを使う場合はコメントのままで良い）
 # set --global fish_key_bindings fish_default_key_bindings
 
-# ---- テーマ / 色設定 ----
-set --global fish_color_cancel -r
-set --global fish_color_cwd_root red
-set --global fish_color_history_current --bold
-set --global fish_color_host_remote yellow
-set --global fish_color_selection white --bold --background=brblack
-set --global fish_color_status red
-set --global fish_color_user brgreen
-set --global fish_color_valid_path --underline
-
-set --global fish_pager_color_completion
-set --global fish_pager_color_description B3A06D yellow
-set --global fish_pager_color_prefix white --bold --underline
-set --global fish_pager_color_progress brwhite --background=cyan
-set --global fish_pager_color_selected_background -r
-# -------------------------
 
 # --------------------------------------------------------
 # EDITOR 設定
